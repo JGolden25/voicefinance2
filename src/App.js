ca4ffa6c04a35467ed8d83c710cbc2da91cdf591
Joseph Chretien-Golden <joechre@gmail.com>
2022-01-15 13:55:28 -0600

    add grid

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import Details from './components/Details/Details';
 import useStyles from './styles';
 
 const App = () => {
-
+    const classes = useStyles();
   return (
     <div>
-      <Grid container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}}>
+      <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}}>
         <Grid item xs={12} sm={4}>
           <Details title="Income" />
         </Grid>
